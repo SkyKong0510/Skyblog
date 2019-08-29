ebbb0a0796b2640ba93bdaa9bb768e88628d5b68
SkyKong0510 <1036892514@qq.com>
2019-08-29 10:57:27 +0800

    03time

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,12 @@
 from flask import Flask
+from config import Config#从config模块导入Config类
+from flask_sqlalchemy import SQLAlchemy#从包中导入类
+from flask_migrate import Migrate
 
 app = Flask(__name__)
+app.config.from_object(Config)
 
-from app import routes
+db = SQLAlchemy(app)#数据库对象
+migrate = Migrate(app, db)#迁移引擎对象
+
+from app import routes,models
